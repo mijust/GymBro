d1cc78e5a97d0137f59ef1810799f3ece10a5c19
mjustus <michael.justus@outlook.de>
2024-02-04 22:42:47 +0100

    desing maybe?

--- TrainHelp/ContentView.swift
+++ TrainHelp/ContentView.swift
@@ -1,60 +1,119 @@
-//
-//  ContentView.swift
-//  TrainHelp
-//
-//  Created by Michael Justus on 04.02.24.
-//
-
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            
             HStack {
-                Color.gray
                 Label("\(currentDate())", systemImage: "calendar")
                     .padding()
                     .font(.headline)
+                    .foregroundColor(.accentColor) // Verwende die Akzentfarbe
+                    .padding(.bottom, 10) // Hier wird ein Padding zum oberen Rand hinzugefügt
+
+                
+                Spacer()
+                
+                Button(action: {
+                    // Hier kannst du den Code für die Aktion hinzufügen
+                    print("Button wurde gedrückt!")
+                }) {
+                    Image(systemName: "plus.app")
+                        .resizable()
+                        .foregroundColor(.accentColor) // Verwende die Akzentfarbe
+                        .frame(width: 30, height: 30)
+                        .padding(.bottom, 10) // Hier wird ein Padding zum oberen Rand hinzugefügt
+
+                }
+                .padding()
             }
-            .background(Color.gray)
             .frame(height: 40)
+            .background(Color("Complementary") , ignoresSafeAreaEdges: .top)
+            
             
             HStack {
-                Color.gray
+                ScrollView {
+                    VStack {
+                        ZStack {
+                            Color.accentColor
+                                .padding()
+                            Image("Bizeps")
+                                .resizable()
+                                .frame(width:300 , height:300)
+                                .background(Color.white)
+                                .cornerRadius(6)
+                        }
+                        .frame(height: 400)
+                        .cornerRadius(60)
+                        
+                        ZStack {
+                            Color.accentColor
+                                .padding()
+                            Image("DeadLift")
+                                .resizable()
+                                .frame(width:300 , height:300)
+                                .background(Color.white)
+                                .cornerRadius(6)
+                        }
+                        .frame(height: 400)
+                        .cornerRadius(60)
+                        
+                        ZStack {
+                            Color.accentColor
+                                .padding()
+                            Image("Legraises")
+                                .resizable()
+                                .frame(width:300 , height:300)
+                                .background(Color.white)
+                                .cornerRadius(6)
+                        }
+                        .frame(height: 400)
+                        .cornerRadius(60)
+                    }
+                }
             }
-            .background(Color.gray)
-            .padding(.vertical)
-
+            .background(Color.white)
             
             HStack {
                 Spacer()
+                
                 VStack{
-                    Color.orange
-                        .padding()
+                    Color.accentColor
+                        .cornerRadius(10)
+                        .frame(width:75 , height: 75)
                 }
-                Spacer()
+                .padding(.top, 10) // Hier wird ein Padding zum oberen Rand hinzugefügt
 
-                VStack{
-                    Color.blue
-                        .padding()
-                }
+                
                 Spacer()
-
+                
                 VStack{
-                    Color.black
-                        .padding()
+                    Color.accentColor
+                        .cornerRadius(10)
+                        .frame(width:75 , height: 75)
                 }
+                .padding(.top, 10) // Hier wird ein Padding zum oberen Rand hinzugefügt
+                
                 Spacer()
-
+                
                 VStack{
-                    Color.green
-                        .padding()
+                    Color.accentColor
+                        .cornerRadius(10)
+                        .frame(width:75 , height: 75)
                 }
+                .padding(.top, 10) // Hier wird ein Padding zum oberen Rand hinzugefügt
+                
+                Spacer()
+                
+                VStack{
+                    Color.accentColor
+                        .cornerRadius(10)
+                        .frame(width:75 , height: 75)
+                }
+                .padding(.top, 10) // Hier wird ein Padding zum oberen Rand hinzugefügt
+                
                 Spacer()
             }
-            .background(Color.gray)
-            .frame(height: 90)
+            .background(Color("Complementary") , ignoresSafeAreaEdges: .bottom)
         }
     }
 }
